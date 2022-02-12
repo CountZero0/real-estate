7ca1d4f1715acbd55cbe68485bb3f820ba7f4ff6
Konstantin <akechijinsai@gmail.com>
2022-02-12 19:13:10 +0300

    Normalize Flat.phonenumbers into Flat.owner_pure_phone

--- property/models.py
+++ property/models.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+
 from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    owner_pure_phone = PhoneNumberField("Нормальзованный номер владельца", blank=True,
-                                        null=True, db_index=True)
+    owner_pure_phone = PhoneNumberField(
+        "Нормальзованный номер владельца",
+        blank=True, null=True, db_index=True)
     new_building = models.NullBooleanField()
     created_at = models.DateTimeField(
         'Когда создано объявление',
